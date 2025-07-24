3d8f1f281ae65e80b4cab3257ae8b0e1d57a0f07
Chiziaruhoma Ogbonda <chiziaruhoma@gmail.com>
2025-07-24 23:02:30 +0100

    feat: updated profile

---
+++ app/logos/eleven-labs.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+
+const ElevenLabs = (props: SVGProps<SVGSVGElement>) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="1em"
+        height="1em"
+        viewBox="0 0 55 89"
+        fill="none"
+        {...props}
+    >
+        <path
+            d="M0 0H18.413V88.5124H0V0Z"
+            fill="currentColor"
+        />
+        <path
+            d="M36.5788 0H54.9917V88.5124H36.5788V0Z"
+            fill="currentColor"
+        />
+    </svg>
+);
+
+export default ElevenLabs;
